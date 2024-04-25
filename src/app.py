19e315ab57ca485e1c99a4b3b4097377f2bfa730
Mohamedadlyi <95952375+Mohamedadlyi@users.noreply.github.com>
2024-04-25 09:42:47 +0200

    Update app.py

--- src/app.py
+++ src/app.py
@@ -6,6 +6,9 @@ import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output
 import plotly.graph_objects as go
+import base64
+import io
+
 
 # Read the data
 df = pd.read_csv('transactions_2024_categorized.csv', index_col=False)
@@ -16,11 +19,30 @@ df['Month Name'] = pd.to_datetime(df['Date']).dt.strftime("%b")
 # Define functions to create pie charts
 def make_pie_chart(df, year, label):
     sub_df = df[(df['Expense/Income'] == label) & (df['Year'] == year)]
+
     if label == 'Income':
         fig = px.pie(sub_df, values='Amount (EGP)', names='Name / Description', title=f'{label} Breakdown {year}')
     else:
         fig = px.pie(sub_df, values='Amount (EGP)', names='Category', title=f'{label} Breakdown {year}')
+
+        # Calculate savings percentage for expenses
+        if label == 'Expense':
+            total_income = df[(df['Expense/Income'] == 'Income') & (df['Year'] == year)]['Amount (EGP)'].sum()
+            total_expense = sub_df['Amount (EGP)'].sum()
+            savings = total_income - total_expense
+            savings_percentage = (savings / total_income) * 100 if total_income > 0 else 0
+
+            # Add annotation for savings percentage
+            fig.update_layout(
+                annotations=[dict(
+                    text=f'Savings: {savings_percentage:.2f}%',
+                    x=0.8, y=0.2, showarrow=False, font_size=16, font_color='white'
+                )],
+                title=f'Expense Breakdown {year}'
+            )
+
     return fig
+
 
 # Define function to create bar chart
 def make_bar_chart(df, year, label):
@@ -29,14 +51,50 @@ def make_bar_chart(df, year, label):
     fig = px.bar(monthly_totals, x='Month Name', y='Amount (EGP)', title=f'{label} per Month {year}')
     return fig
 
+# Define function to create line chart for expenses per month across all years
+def make_line_chart_all_years(df):
+    expense_df = df[df['Expense/Income'] == 'Expense']
+    monthly_expenses = expense_df.groupby(['Year', 'Month'])['Amount (EGP)'].sum().reset_index()
+    fig = px.line(monthly_expenses, x='Month', y='Amount (EGP)', color='Year',
+                  labels={'Amount (EGP)': 'Total Expenses (EGP)', 'Month': 'Month'})
+    fig.update_layout(title='Expenses per Month Across Years', xaxis=dict(tickmode='linear'))
+    return fig
+
 # Create Dash app
 app = dash.Dash(__name__)
 server = app.server
-
 # Define app layout using Dash components
 app.layout = html.Div([
     html.H1("Financial Analysis Dashboard"),
-    
+    # dcc.Graph(id='line-chart', figure=make_line_chart_all_years(df)),
+    html.Div(
+        [
+            dcc.Upload(
+                id='upload-CSV-data',
+                children=html.Div([
+                    'Drag and Drop or ',
+                    html.A('Select Files')
+                ]),
+                style={
+                    'width': '100%',
+                    'height': '100%',
+                    'display': 'flex',
+                    'justify-content': 'center',
+                    'align-items': 'center',
+                    'borderWidth': '1px',
+                    'borderStyle': 'dashed',
+                    'borderRadius': '5px',
+                    'borderColor': '#999',
+                    'textAlign': 'center',
+                    'background-color': '#444',
+                    'color': '#ccc'
+                },
+                # Allow multiple files to be uploaded
+                multiple=False
+            ),
+        ],
+        style={'width': '100px', 'height': '100px'}  # Adjust dimensions of the upload button container
+    ),
     # Dropdown to select year
     dcc.Dropdown(
         id='year-dropdown',
@@ -59,24 +117,67 @@ app.layout = html.Div([
     html.Div(id='charts-div')
 ])
 
-# Callback to update charts based on dropdown and tab selection
+
 @app.callback(
     Output('charts-div', 'children'),
     [Input('year-dropdown', 'value'),
-     Input('tabs', 'value')]
+     Input('tabs', 'value'),
+     Input('upload-CSV-data', 'contents'),
+     Input('upload-CSV-data', 'filename')]
 )
-def update_charts(year, tab):
-    if tab == 'income-tab':
-        pie_chart_fig = make_pie_chart(df, year, 'Income')
-        bar_chart_fig = make_bar_chart(df, year, 'Income')
-    elif tab == 'expense-tab':
-        pie_chart_fig = make_pie_chart(df, year, 'Expense')
-        bar_chart_fig = make_bar_chart(df, year, 'Expense')
-    
-    return html.Div([
-        dcc.Graph(figure=pie_chart_fig),
-        dcc.Graph(figure=bar_chart_fig)
-    ])
+def update_data_and_charts(year, tab, contents, filename):
+    # Check if a file was uploaded
+    if contents is not None:
+        content_type, content_string = contents.split(',')
+        decoded = base64.b64decode(content_string)
+        try:
+            if 'csv' in filename:
+                # Read the uploaded CSV file into a DataFrame
+                new_df = pd.read_csv(filename, index_col=False)
+
+                
+                # Update the existing DataFrame with the uploaded data
+                global df
+                df = new_df
+
+                # Regenerate charts with updated data
+                if tab == 'income-tab':
+                    pie_chart_fig = make_pie_chart(df, year, 'Income')
+                    bar_chart_fig = make_bar_chart(df, year, 'Income')
+                elif tab == 'expense-tab':
+                    pie_chart_fig = make_pie_chart(df, year, 'Expense')
+                    bar_chart_fig = make_bar_chart(df, year, 'Expense')
+
+                # Return updated charts
+                return html.Div([
+                    html.Div([
+                        dcc.Graph(figure=pie_chart_fig)
+                    ], style={'width': '50%', 'display': 'inline-block'}),
+                    html.Div([
+                        dcc.Graph(figure=bar_chart_fig)
+                    ], style={'width': '50%', 'display': 'inline-block'})
+                ])
+        except Exception as e:
+            print(str(e))
+            return html.Div(['There was an error processing this file.'])
+    else:
+        # No file uploaded, return existing charts
+        if tab == 'income-tab':
+            pie_chart_fig = make_pie_chart(df, year, 'Income')
+            bar_chart_fig = make_bar_chart(df, year, 'Income')
+        elif tab == 'expense-tab':
+            pie_chart_fig = make_pie_chart(df, year, 'Expense')
+            bar_chart_fig = make_bar_chart(df, year, 'Expense')
+
+        return html.Div([
+            html.Div([
+                dcc.Graph(figure=pie_chart_fig)
+            ], style={'width': '50%', 'display': 'inline-block'}),
+            html.Div([
+                dcc.Graph(figure=bar_chart_fig)
+            ], style={'width': '50%', 'display': 'inline-block'})
+        ])
+
 
 if __name__ == '__main__':
     app.run_server(debug=True)
